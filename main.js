4c66195b9f4386a1c14669670930a06786a88c90
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2022-08-17 14:34:53 -0500

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+let characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
   "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c",
   "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
   "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~",
@@ -10,7 +10,15 @@ let password2 = document.getElementById("password2");
 
 function generatePassword() {
   for (let i = 0; i < 15; i++) {
-    let charactersRandom = Math.floor(Math.random() * characters.length);
-    console.log(characters[charactersRandom]);
+    let charactersRandom1 = Math.floor(Math.random() * characters.length);
+    let charactersRandom2 = Math.floor(Math.random() * characters.length);
+    password1.innerHTML += characters[charactersRandom1];
+    password2.innerHTML += characters[charactersRandom2];
   }
+  //clearPassword();
+}
+
+function clearPassword() {
+  password1.textContent = "f"
+  password2.textContent = "d"
 }
