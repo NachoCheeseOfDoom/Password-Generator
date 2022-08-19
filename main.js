893bc017e0c72a6fcacd42e848f786ee2f873b64
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2022-08-18 21:10:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,59 @@
 let characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
   "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c",
   "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+  "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+let symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
+  "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"]
+
+let charactersAndSymbols = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+  "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c",
+  "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
   "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~",
   "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[",
-  "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
+  "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"]
 
 let password1 = document.getElementById("password1");
 let password2 = document.getElementById("password2");
 let passwordLength = document.getElementById("password-length");
 let clicked = false;
 
-function generatePassword() {
 
+
+function generatePassword() {
+  if (document.getElementById("radio1").checked) {
+    whithSymbols();
+  }
+  else if (document.getElementById("radio2").checked) {
+    whithoutSymbols()
+  }
+}
+
+function whithSymbols() {
+  clicked = true
+  clearPassword();
+  for (let i = 0; i < passwordLength.value; i++) {
+    let charactersRandom1 = Math.floor(Math.random() * charactersAndSymbols.length);
+    let charactersRandom2 = Math.floor(Math.random() * charactersAndSymbols.length);
+    password1.innerHTML += charactersAndSymbols[charactersRandom1];
+    password2.innerHTML += charactersAndSymbols[charactersRandom2];
+  }
+}
+
+function whithoutSymbols() {
   clicked = true
   clearPassword();
   for (let i = 0; i < passwordLength.value; i++) {
     let charactersRandom1 = Math.floor(Math.random() * characters.length);
     let charactersRandom2 = Math.floor(Math.random() * characters.length);
-    //password1.innerHTML = characters.push(charactersRandom1);
     password1.innerHTML += characters[charactersRandom1];
     password2.innerHTML += characters[charactersRandom2];
   }
-
 }
+
 function clearPassword() {
-  password1.innerHTML = ""
-  password2.innerHTML = ""
+  password1.innerHTML = " "
+  password2.innerHTML = " "
 }
 
 function copy1() {
@@ -55,15 +83,3 @@ function copy2() {
     alert("Nothing Copied");
   }
 }
-function verificarCB() {
-
-  if (document.getElementById("radio1").checked) {
-    alert("Estados Unidos esta seleccionado!");
-  }
-  else if (document.getElementById("radio2").checked) {
-    alert("Mexico esta seleccionado!");
-
-  }
-}
-
-verificarCB();
